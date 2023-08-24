96524660606625bb2d35aa97e294ea2f108018f2
Kal Nascimento <kalebe.nascimento19@gmail.com>
2023-08-24 10:39:35 -0300

    test: config jest

---
+++ src/screens/Home/Timer/index.spec.tsx
@@ -0,0 +1,11 @@
+import { Platform } from 'react-native';
+
+jest.mock('react-native', () => ({
+  Platform: { OS: 'ios' },
+}));
+
+describe('React Native Platform', () => {
+  test("Platform.OS should be 'ios'", () => {
+    expect(Platform.OS).toBe('ios');
+  });
+});
